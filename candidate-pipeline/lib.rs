be6d6c60b1458e60d149af5844f9d6cb54bcfcda
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-03-30 22:11:19 +0530

    Add files via upload

---
+++ candidate-pipeline/lib.rs
@@ -0,0 +1,24 @@
+// Copyright © 2026 Mohammad Faiz
+// Repository: https://github.com/Mohammad-Faiz-Cloud-Engineer/Algorithm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub mod candidate_pipeline;
+pub mod filter;
+pub mod hydrator;
+pub mod query_hydrator;
+pub mod scorer;
+pub mod selector;
+pub mod side_effect;
+pub mod source;
+pub mod util;
